fcdf4000fbaf80dfbc9fc761febec19354ca43af
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 15:29:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Raphael","Donatello", "Michelangelo", Leonardo]
+  ["Raphael","Donatello", "Michelangelo", "Leonardo"]
 end
 
 def add_element_to_end_of_array(array, element)
